adf35fe0653467d8ff054eaa9e93cae407528b49
JosephChege4 <chegejrmungai@gmail.com>
2025-05-16 01:29:26 -0400

    changed the threshold for missing values

--- scripts/clean_utils.py
+++ scripts/clean_utils.py
@@ -5,7 +5,7 @@ def clean_column_names(df: pd.DataFrame) -> pd.DataFrame:
     """
     Standardizes column names to lowercase and underscores.
     """
-    df.columns(
+    df.columns = (
         df.columns.str.strip()
         .str.lower()
         .str.replace(" ", "_")
